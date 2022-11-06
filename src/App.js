782e9be9b7429c43289ca3316ffdcd7b8f371273
robertandreiboboc <andreiboboccibo@gmail.com>
2022-11-06 09:47:56 +0200

    Gen 1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} style={{height: "300px"}} alt="logo" />
         <h1>Generation Salez</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
